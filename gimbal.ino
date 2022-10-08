312f9623e93138c81b73b4e0e4761b833bc16979
unknown <strayed.rat@gmail.com>
2022-10-07 22:30:50 -0700

    servo implementation
    set the servo arm to the current angle of the MPU

--- gimbal.ino
+++ gimbal.ino
@@ -1,21 +1,31 @@
 #include <Wire.h>
+#include <Servo.h>
 
 int MPU = 0x68; //MPU address
 int accelAdd = 0x3B; //acceleration register address
+int pwrAdd = 0x1C; //power management register address
+int servPort = 10; //the arduino digital pin for the servo
 float accelSens = 16384.0; //the sensitivity chosen for the acceleration
 int16_t aX, aY, aZ; //raw acceleration values from the MPU
 float gX, gY, gZ; //adjusted values from the raw MPU input
+Servo servo;
+float angle;
 
 void setup() {
   mpuConfig();
+  servo.attach(servPort);
   Serial.begin(9600);
+
 }
 
 void loop() {
   getAccel();
   printData();
+  angle = atan2(gY,gX) / PI * 180;
+  servo.write(angle);
   delay(200);
 }
+
 
 void mpuConfig() {
   Wire.beginTransmission(MPU); 
@@ -23,7 +33,7 @@ void mpuConfig() {
   Wire.write(0b00000000); //set all the bits for pwr to 0 to wake mpu
   Wire.endTransmission();  
   Wire.beginTransmission(MPU); 
-  Wire.write(0x1C); 
+  Wire.write(pwrAdd); 
   Wire.write(0b00000000); //set all the bits for accel to 0 for 2g range
   Wire.endTransmission(); 
 }
@@ -39,11 +49,11 @@ void getAccel() {
     then use logical or to fill the bottom 8 bits
   */
 
-  aX= Wire.read() << 8 | Wire.read(); 
+  aX = Wire.read() << 8 | Wire.read(); 
   gX = aX / accelSens;
   aY = Wire.read() << 8 | Wire.read();
   gY = aY / accelSens;
-  aZ= Wire.read() << 8 |Wire.read();
+  aZ = Wire.read() << 8 | Wire.read();
   gZ = aZ / accelSens;
 }
 
